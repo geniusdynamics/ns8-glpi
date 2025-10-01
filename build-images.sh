77e1de0ae4a9a8ae383fe636d337e28ba7427b95
Kemboi Elvis <kemboielvis22@gmail.com>
2025-10-02 01:23:35 +0300

    feat: v11

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="glpi"
-app_version="10.0.18"
+app_version="11"
 # Create a new empty container image
 container=$(buildah from scratch)
 
